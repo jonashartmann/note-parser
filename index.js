dd04795cf6ece2681df89fa2f8239d3cb5d08153
Jonas Hartmann <jonasharty@gmail.com>
2015-09-05 22:17:32 +0200

    Add options and parameters

--- index.js
+++ index.js
@@ -1,41 +1,70 @@
 #!/usr/bin/env node
 
-var fs  = require("fs");
-var colors = require('colors');
-var lazy    = require("lazy");
-var Parser = require('./parser');
-var Writer = require('./writer');
+var fs      = require('fs');
+var colors  = require('colors');
+var lazy    = require('lazy');
+var app     = require('commander');
+var Parser  = require('./parser');
+var Writer  = require('./writer');
 
+function run (filename) {
+    var color = app.color || 'green';
+    var parser = new Parser();
+    var writer = new Writer(process.stdout);
+    var readStream = filename ?
+        fs.createReadStream(filename) :
+        process.stdin;
 
-var parser = new Parser();
-var writer = new Writer(process.stdout);
+    new lazy(readStream)
+        .lines
+        .map(String)
+        .map(parser.countLine.bind(parser))
+        .map(parser.tokenize.bind(parser))
+        .forEach(function (line) {
+            var toBuf = parser.currentLine + ': ' +  line;
+            if (parser.hasMention()) {
+                toBuf = toBuf[color];
+            }
+            writer.buffer(toBuf.replace(/[ \r\n]+$/g, '\n'));
+        })
+        .join(function (lines) {
+            var tasks = parser.tasks;
 
-new lazy(process.stdin)
-    .lines
-    .map(String)
-    .map(parser.countLine.bind(parser))
-    .map(parser.tokenize.bind(parser))
-    .forEach(function (line) {
-		var toBuf = parser.currentLine + ': ' +  line;
-		if (parser.hasMention()) {
-			toBuf = toBuf.green;
-		}
-		writer.buffer(toBuf.replace(/[ \r\n]+$/g, '\n'));
+            writer
+                .writeLine(' ')
+                .writeLine('==========================='[color]);
+            for (var task in tasks) {
+                writer
+                    .write(task[color].bold)
+                    .write(' => ')
+                    .writeLine(colors[color].bold(tasks[task]));
+            }
+            writer
+                .writeLine('==========================='[color])
+                .writeLine()
+                .flush();
+        });
+}
+
+var packageFile = fs.readFileSync('package.json');
+var version = /(\d.\d.\d)+/.exec(packageFile)[0];
+
+app
+    .usage('[options] <filename>')
+    .version(version)
+    .option('-c, --color [color]', 'Color to use in the output')
+    .on('--help', function() {
+          console.log('  Examples:');
+          console.log('');
+          console.log('    $ noteparser notes.txt');
+          console.log('    $ noteparser notes.txt > out.txt');
+          console.log('');
     })
-    .join(function (lines) {
-		var tasks = parser.tasks;
-
-		writer
-			.writeLine(' ')
-			.writeLine('==========================='.green);
-		for (var task in tasks) {
-			writer
-				.write(task.green.bold)
-				.write(' => ')
-				.writeLine(colors.green.bold(tasks[task]));
-		}
-		writer
-			.writeLine('==========================='.green)
-			.writeLine()
-			.flush();
+    .action(function (filename) {
+        app.file = filename;
     });
+
+// Hack the process name
+process.argv[1] = 'noteparser';
+app.parse(process.argv);
+run(app.file);
